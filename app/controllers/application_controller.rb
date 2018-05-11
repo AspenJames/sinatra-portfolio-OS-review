3890398c35eb1bd7c65baf551c5b8741997d2213
Aspen James <aspenjames@tqca.org>
2018-05-11 12:11:31 -0700

    init all controllers

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "Basidiomycota"
+    use Rack::Flash
+  end
+end
---
+++ app/controllers/operating_system_controller.rb
@@ -0,0 +1,3 @@
+class OperatingSystemController < ApplicationController
+
+end
