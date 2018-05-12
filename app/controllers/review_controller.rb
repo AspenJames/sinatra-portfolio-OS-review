d3103449524edd347bb5e38fb55c0a1005168263
Aspen James <aspenjames@tqca.org>
2018-05-11 19:58:06 -0700

    build basic new review route

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,3 +1,16 @@
 class ReviewController < ApplicationController
 
+  get '/reviews/new' do
+    @os = OperatingSystem.all
+    @user = Helper.current_user(session)
+
+    erb :'/reviews/new'
+  end
+
+  post '/reviews' do
+    review = Review.create(params["review"])
+    review.operating_system = OperatingSystem.create(params["os"]) unless params["os"]["name"].empty?
+    redirect :"/reviews#{review.id}"
+  end
+
 end
