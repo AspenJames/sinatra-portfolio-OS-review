e1a7c2fd27ad67b88c053e68cfb62513f2e3e3f3
Aspen James <aspenjames@tqca.org>
2018-05-11 19:14:32 -0700

    populate environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,11 @@
 #environment
+require 'bundler/setup'
+Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3"
+  :database => "db/database.sqlite"
+)
+
+require_all 'app'
+require_all 'public'
