c833089babd063132c37c88a72b404cae0938051
Aspen James <aspenjames@tqca.org>
2018-05-13 13:02:44 -0700

    restrict /operating_systems/ routes to logged-in users

--- app/controllers/operating_system_controller.rb
+++ app/controllers/operating_system_controller.rb
@@ -1,14 +1,21 @@
 class OperatingSystemController < ApplicationController
 
   get :'/operating_systems' do
-    @os = OperatingSystem.all
-    erb :"operating_systems/index"
+    if Helper.logged_in?(session)
+      @os = OperatingSystem.all
+      erb :"operating_systems/index"
+    else
+      redirect :'/'
+    end
   end
 
   get :'/operating_systems/:id' do
-    @os = OperatingSystem.find_by(id: params[:id])
-
-    erb :'/operating_systems/show'
+    if Helper.logged_in?(session)
+      @os = OperatingSystem.find_by(id: params[:id])
+      erb :'/operating_systems/show'
+    else
+      redirect :'/'
+    end
   end
 
 end
