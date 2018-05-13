053d24ef0f2c43ce16130b1dbf40994847d6b8f8
Aspen James <aspenjames@tqca.org>
2018-05-12 22:13:35 -0700

    build basic index and show routes to users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,13 @@
 class UserController < Sinatra::Base
-  
+
+  get '/users' do
+    @users = User.all
+    erb :'/users/index'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
 end
