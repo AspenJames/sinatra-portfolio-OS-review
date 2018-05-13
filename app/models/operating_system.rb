50899fb909856a868c9d3ff90f6e113674d41fa1
Aspen James <aspenjames@tqca.org>
2018-05-12 22:09:01 -0700

    add slug methods to user and operating system classes

--- app/models/operating_system.rb
+++ app/models/operating_system.rb
@@ -1,5 +1,13 @@
 class OperatingSystem < ActiveRecord::Base
 
+  def slug
+    self.name.gsub(/\s/, "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|i| i.slug == slug}
+  end
+
   has_many :reviews
   has_many :users, through: :reviews
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+
+  def slug
+    self.name.gsub(/\s/, "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|i| i.slug == slug}
+  end
+  
   include ActiveModel::Validations
   has_secure_password
 
