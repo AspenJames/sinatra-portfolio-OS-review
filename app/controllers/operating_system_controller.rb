272b1ed3b245c82fe0ac5f14b103cab69a5dd3fb
Aspen James <aspenjames@tqca.org>
2018-05-12 22:05:15 -0700

    build operating system index and show routes

--- app/controllers/operating_system_controller.rb
+++ app/controllers/operating_system_controller.rb
@@ -1,3 +1,14 @@
 class OperatingSystemController < ApplicationController
 
+  get :'/operating_systems' do
+    @os = OperatingSystem.all
+    erb :"operating_systems/index"
+  end
+
+  get :'/operating_systems/:id' do
+    @os = OperatingSystem.find_by(id: params[:id])
+
+    erb :'/operating_systems/show'
+  end
+
 end
